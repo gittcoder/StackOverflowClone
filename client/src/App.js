d86de605fe017e94c6053a2d7c49a2eeb37db302
gittcoder <adityaavinash.n2020@vitstudent.ac.in>
2023-01-10 17:39:10 +0530

    production

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import {useEffect} from 'react'
 import './App.css';
-import Navbar from './components/Navbar/Navbar.js'  
 import {BrowserRouter as Router} from 'react-router-dom';
 import Routes from './BRoutes';
 import {useDispatch} from 'react-redux'
 import {fetchAllQuestions} from './actions/question'
 import { fetchAllUsers } from './actions/users';
-
+import Navbar from './components/Navbar/Navbar.js' ;
 
 
 const App=()=> {
