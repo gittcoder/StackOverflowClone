77a1f3f329c365183e9d610024a72861319ff1e5
gittcoder <adityaavinash.n2020@vitstudent.ac.in>
2023-01-10 17:51:22 +0530

    comeon

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import {useEffect} from 'react'
 import './App.css';
+import Navbar from './components/Navbar/Navbar'  
 import {BrowserRouter as Router} from 'react-router-dom';
 import Routes from './BRoutes';
 import {useDispatch} from 'react-redux'
 import {fetchAllQuestions} from './actions/question'
 import { fetchAllUsers } from './actions/users';
-import Navbar from './components/Navbar/Navbar.js' ;
+
 
 
 const App=()=> {
